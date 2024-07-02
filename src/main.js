306929c79a70b8644cfeb9776c92de20f5ed9061
pchee <pchee1489@naver.com>
2024-07-03 00:27:20 +0900

    라우터 설치

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import router from '@/router';
 
-createApp(App).mount('#app');
+createApp(App).use(router).mount('#app');
 
 import 'bootstrap/dist/js/bootstrap';
